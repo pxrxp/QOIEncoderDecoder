7134572a5b4825cad7d2c407e628c9755a8732d7
pxrxp <187556078+pxrxp@users.noreply.github.com>
2025-11-24 18:59:55 +0545

    Add QOIHeader in decoder

--- src/decoder/qoi_decoder.rs
+++ src/decoder/qoi_decoder.rs
@@ -1,8 +1,45 @@
+use crate::errors::QOIError;
+use image::DynamicImage;
 use std::fs;
 
-use image::DynamicImage;
+struct QOIHeader {
+    width: u32,
+    height: u32,
+    channels: u8,
+    colorspace: u8,
+}
 
-use crate::errors::QOIError;
+impl QOIHeader {
+    fn new<'a, I>(iter: &mut I) -> Result<Self, QOIError>
+    where
+        I: Iterator<Item = &'a u8>,
+    {
+        let magic_chunks = chunk(iter, 4);
+        if magic_chunks != b"qoif" {
+            return Err(QOIError::ImageDecodeError);
+        }
+
+        let width = u32::from_be_bytes(
+            chunk(iter, 4)
+                .try_into()
+                .map_err(|_| QOIError::ImageDecodeError)?,
+        );
+        let height = u32::from_be_bytes(
+            chunk(iter, 4)
+                .try_into()
+                .map_err(|_| QOIError::ImageDecodeError)?,
+        );
+        let channels = *iter.next().ok_or(QOIError::ImageDecodeError)?;
+        let colorspace = *iter.next().ok_or(QOIError::ImageDecodeError)?;
+
+        Ok(Self {
+            width,
+            height,
+            channels,
+            colorspace,
+        })
+    }
+}
 
 pub struct DecodedImage {
     image: Option<DynamicImage>,
@@ -17,23 +54,7 @@ pub fn decode_file(image_path: &str) -> Result<DecodedImage, QOIError> {
 pub fn decode(image_bytes: &Vec<u8>) -> Result<DecodedImage, QOIError> {
     let mut iter = image_bytes.iter();
 
-    let magic_chunks = chunk(&mut iter, 4);
-    let width = u32::from_be_bytes(
-        chunk(&mut iter, 4)
-            .try_into()
-            .map_err(|_| QOIError::ImageDecodeError)?,
-    );
-    let height = u32::from_be_bytes(
-        chunk(&mut iter, 4)
-            .try_into()
-            .map_err(|_| QOIError::ImageDecodeError)?,
-    );
-    let channels = iter.next().unwrap();
-    let colorspace = iter.next().unwrap();
-
-    assert_eq!(magic_chunks, b"qoif");
-    assert!(*channels == 3 || *channels == 4);
-    assert!(*colorspace == 0 || *colorspace == 1);
+    let header = QOIHeader::new(&mut iter)?;
 
     Ok(DecodedImage {
         image: None,
